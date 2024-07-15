11141307383f1b40dec0ba59350e07488f29e346
Jay-luozhijie <549758121t@gmail.com>
2024-07-15 20:41:38 +0800

    change server.js

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.use('/api/users', userRouter);
 
 
 const port = process.env.PORT || 5000
-app.listen(port, ()=>{
+app.listen(port, '0.0.0.0', ()=>{
     console.log(`Serve at http://localhost:${port}`);
 });
