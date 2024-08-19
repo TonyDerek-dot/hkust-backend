eca67df51c2b36fdeb4c8730cd28ba9fe698308b
TonyDerek-dot <yu1429422734@gmail.com>
2024-08-19 18:34:27 +0800

    upload

--- server.js
+++ server.js
@@ -3,8 +3,23 @@ import cors from 'cors';
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRouter from './routers/userRouter.js';
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
 
 dotenv.config();
+
+// 设置文件存储路径
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, '/var/www/uploads');
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    }
+  });
+  
+const upload = multer({ storage: storage });
 
 const app = express();
 
@@ -32,7 +47,14 @@ app.use(cors());
 
 app.use('/api/users', userRouter);
 
-
+// 处理文件上传的POST请求，路径包含 /api/ 前缀
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  try {
+      res.status(200).send({ message: "File uploaded successfully" });
+  } catch (error) {
+      res.status(500).send({ error: "Failed to upload file" });
+  }
+});
 
 const port = process.env.PORT || 5000
 app.listen(port, ()=>{
